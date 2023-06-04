780737e6ece0d67cfdbfddca50b12058b265bd8a
Lucas Ferreira <casferreiraa@gmail.com>
2023-06-04 05:58:34 -0300

    config(apolloclient): config and request with long and lat params

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,8 @@
 
+import { getClient } from '@/apollo-client';
+import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
+
+
 type Props = {
   params: {
     city: string;
@@ -7,11 +11,24 @@ type Props = {
   }
 }
 
+export default async function WeatherPage({ params: { city, lat, long } }: Props) {
 
-export default function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      latitude: lat,
+      longitude: long,
+      timezone: "GMT",
+    }
+  });
 
+  const result:Root = data.myQuery;
+  console.log('result', result);
   return (
     <div>
+
     </div>
   )
 
